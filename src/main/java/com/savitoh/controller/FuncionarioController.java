5f8fab7863c17b4c92379f2a16a276f6dfabd201
savitoh <savioraires10@gmail.com>
2018-12-07 19:48:20 -0200

    Validação BackEnd Funcionários

--- src/main/java/com/savitoh/controller/FuncionarioController.java
+++ src/main/java/com/savitoh/controller/FuncionarioController.java
@@ -3,6 +3,9 @@ package com.savitoh.controller;
 import java.time.LocalDate;
 import java.util.List;
 
+import javax.validation.Valid;
+
+import com.savitoh.controller.validator.FuncionarioValidator;
 import com.savitoh.domain.Cargo;
 import com.savitoh.domain.Funcionario;
 import com.savitoh.domain.UF;
@@ -13,7 +16,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -30,6 +36,11 @@ public class FuncionarioController {
 
 	@Autowired
 	private CargoService cargoService;
+
+	@InitBinder
+	public void initBinder(WebDataBinder binder) {
+		binder.addValidators(new FuncionarioValidator());
+	}
 	
 	@GetMapping("/cadastrar")
 	public String cadastrar(Funcionario funcionario) {
@@ -44,7 +55,12 @@ public class FuncionarioController {
 	}
 
 	@PostMapping("/salvar")
-	public String salvar(Funcionario funcionario, RedirectAttributes attr){
+	public String salvar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr){
+
+		if(result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
+
 		funcionarioService.salvar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário inserido com sucesso");
 		return "redirect:/funcionarios/cadastrar";
@@ -57,7 +73,12 @@ public class FuncionarioController {
 	}
 
 	@PostMapping("/editar")
-	public String editar(Funcionario funcionario, RedirectAttributes attr){
+	public String editar(@Valid Funcionario funcionario, BindingResult result, RedirectAttributes attr){
+
+		if(result.hasErrors()) {
+			return "/funcionario/cadastro";
+		}
+		
 		funcionarioService.editar(funcionario);
 		attr.addFlashAttribute("success", "Funcionário editado com sucesso");
 		return "redirect:/funcionarios/cadastrar";
--- src/main/java/com/savitoh/domain/Funcionario.java
+++ src/main/java/com/savitoh/domain/Funcionario.java
@@ -10,6 +10,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.Size;
 
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.format.annotation.NumberFormat;
@@ -22,13 +27,17 @@ import org.springframework.format.annotation.NumberFormat.Style;
 @Table(name = "FUNCIONARIOS")
 public class Funcionario extends AbstractEntity<Long> {
 	
+	@NotBlank
+	@Size(min = 3, max = 255)
 	@Column(nullable = false, unique = true)
 	private String nome; 
 	
+	@NotNull
 	@NumberFormat(style = Style.CURRENCY, pattern = "#,##0.00")
 	@Column(nullable = false, columnDefinition = "DECIMAL(7,2) DEFAULT 0.00")
 	private BigDecimal salario;
-	
+
+	@PastOrPresent(message = "{PastOrPresent.funcionario.dataEntrada}")
 	@DateTimeFormat(iso = ISO.DATE)
 	@Column(name = "data_entrada", nullable = false, columnDefinition = "DATE")
 	private LocalDate dataEntrada;
@@ -37,10 +46,12 @@ public class Funcionario extends AbstractEntity<Long> {
 	@Column(name = "data_saida", columnDefinition = "DATE")
 	private LocalDate dataSaida;
 	
+	@Valid
 	@OneToOne(cascade = CascadeType.ALL)
 	@JoinColumn(name = "endereco_id_fk")
 	private Endereco endereco;
 	
+	@NotNull(message = "{NotNull.funcionario.cargo}")
 	@ManyToOne
 	@JoinColumn(name = "cargo_id_fk")
 	private Cargo cargo;
